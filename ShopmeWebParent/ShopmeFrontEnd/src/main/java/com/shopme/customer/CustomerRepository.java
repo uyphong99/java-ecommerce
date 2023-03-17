5b5bcdee53053fca6f1b5ade56231ac3946ede6a
Phong Uy <uycuno1@outlook.com.vn>
2023-03-17 14:01:36 +0700

    Dev customer's repository, write test for repository

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.shopme.customer;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopme.common.entity.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+	@Query("SELECT c FROM Customer c WHERE c.email = ?1")
+	public Customer findByEmail(String email);
+	
+	@Query("SELECT c FROM Customer c WHERE c.verificationCode = ?1")
+	public Customer findByVerificationCode(String code);
+}
