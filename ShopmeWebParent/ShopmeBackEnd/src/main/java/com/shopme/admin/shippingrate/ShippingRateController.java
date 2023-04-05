60c9b1514e7fcdeae3b671e513dcc4fc83a6df03
Phong Uy <uycuno1@outlook.com.vn>
2023-04-05 23:01:42 +0700

    Complete basic shipping rate page

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
@@ -1,20 +1,56 @@
 package com.shopme.admin.shippingrate;
 
+import com.shopme.common.entity.ShippingRate;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class ShippingRateController {
 
+    private ShippingRateService shippingRateService;
+
+    @GetMapping("/shipping_rates")
+    public String listFirstPage(Model model) {
+        return listByPage(null, 1, "asc", "id", model);
+    }
+
     @GetMapping("/shipping_rates/page/{pageNumber}")
-    public String listByPage(@PathVariable("pageNumber") Integer pageNumber,
-                             @RequestParam("keyword") String keyword,
+    public String listByPage(@Param("keyword") String keyword,
+                             @PathVariable("pageNumber") Integer pageNumber,
+                             @Param("sortDir") String sortDir,
+                             @Param("sortField") String sortField,
                              Model model) {
+        Page<ShippingRate> shippingRates = shippingRateService.listRatePage(pageNumber, sortField, sortDir, keyword);
+        List<ShippingRate> rates = shippingRates.getContent();
 
+        long startCount = (pageNumber - 1) * ShippingRateService.RATES_PER_PAGE + 1;
+        long endCount = startCount + ShippingRateService.RATES_PER_PAGE - 1;
 
-        return "shipping_reates";
+        if (endCount > shippingRates.getTotalElements()) {
+            endCount = shippingRates.getTotalElements();
+        }
+
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+        model.addAttribute("totalPages", shippingRates.getTotalPages());
+        model.addAttribute("currentPage", pageNumber);
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("totalItems", shippingRates.getTotalElements());
+        model.addAttribute("shippingRates", rates);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", reverseSortDir);
+        model.addAttribute("keyword", keyword);
+
+        return "shipping_rates/shipping_rates";
     }
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateService.java
@@ -1,7 +1,12 @@
 package com.shopme.admin.shippingrate;
 
+import com.shopme.common.entity.ShippingRate;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,4 +16,18 @@ public class ShippingRateService {
     private ShippingRateRepository shippingRateRepository;
 
     public static final Integer RATES_PER_PAGE = 10;
+
+    public Page<ShippingRate> listRatePage(Integer pageNumber, String sortField,
+                                           String sortDir, String keyword) {
+        Sort sort = Sort.by(sortField);
+        sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+
+        Pageable pageable = PageRequest.of(pageNumber - 1, RATES_PER_PAGE, sort);
+
+        if (keyword == null) {
+            return shippingRateRepository.findAll(pageable);
+        }
+
+        return shippingRateRepository.search(keyword, pageable);
+    }
 }
